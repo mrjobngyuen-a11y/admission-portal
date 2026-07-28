dedc136557cf93c77629dee627bb2cc02499b5fb
mrjobngyuen-a11y <mrjobngyuen@gmail.com>
2026-07-28 19:53:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Admission Portal Loaded!");
+const users={
+
+"offer@university.edu":{
+
+password:"123456"
+
+}
+
+}
+
+function login(){
+
+const email=document.getElementById("email").value;
+
+const password=document.getElementById("password").value;
+
+if(users[email]&&users[email].password===password){
+
+alert("Login successful!");
+
+}
+
+else{
+
+document.getElementById("error").innerHTML="Invalid email or password.";
+
+}
+
+}
